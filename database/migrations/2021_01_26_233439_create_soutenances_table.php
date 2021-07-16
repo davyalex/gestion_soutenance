ff2e3811aef7e2d29577d60132a0554e3b98aade
davyalex <alexkouamelan96pv@gmail.com>
2021-07-16 09:27:17 +0000

    modify migration tablesoutenances

--- database/migrations/2021_01_26_233439_create_soutenances_table.php
+++ database/migrations/2021_01_26_233439_create_soutenances_table.php
@@ -17,10 +17,15 @@ class CreateSoutenancesTable extends Migration
             $table->id();  
             $table->date('date_soutenance');  
             $table->time('heure_soutenance');          
-            $table->foreignId('salle_id')->constrained()->onDelete('cascade');
+            // $table->foreignId('salle_id')->constrained()->onDelete('cascade');
             // $table->foreignId('jury_id')->constrained()->onDelete('cascade');
-            $table->foreignId('etudiant_id')->references('id')->constrained()->onDelete('cascade');              
+            // $table->foreignId('etudiant_id')->references('id')->constrained()->onDelete('cascade');              
             $table->string('num_insc'); 
+            $table->unsignedInteger('etudiant_id');
+            $table->foreign('etudiant_id')->references('id')->on('etudiants')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedInteger('salle_id');
+            $table->foreign('salle_id')->references('id')->on('salles')->onDelete('cascade')->onUpdate('cascade');
+           
             $table->timestamps();
         });
     }
